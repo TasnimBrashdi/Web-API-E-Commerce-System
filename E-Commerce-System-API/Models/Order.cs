f59719d50fe1458168075f5d1a88e1a61cb249ab
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-16 13:55:18 +0400

    Add Order Model

---
+++ E-Commerce-System-API/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace E_Commerce_System_API.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("User")]
+        public int UId { get; set; }
+        [JsonIgnore]
+        public virtual User User { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        public decimal TotalAmount { get; set; }    
+
+
+    }
+
+}
+
