17841edb8f19693728cc7b0fe0f8eeef15c015a3
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-16 15:58:56 +0400

    Add Review repo

---
+++ E-Commerce-System-API/Repositories/IReviewRepo.cs
@@ -0,0 +1,13 @@
+﻿using E_Commerce_System_API.Models;
+
+namespace E_Commerce_System_API.Repositories
+{
+    public interface IReviewRepo
+    {
+        void AddReview(Review review);
+        void DeleteReview(int id);
+        List<Review> GetAllReview();
+        Review GetReviewById(int id);
+        void UpdateReview(Review updatedReview);
+    }
+}
---
+++ E-Commerce-System-API/Repositories/ReviewRepo.cs
@@ -0,0 +1,53 @@
+﻿using E_Commerce_System_API.Models;
+
+namespace E_Commerce_System_API.Repositories
+{
+    public class ReviewRepo : IReviewRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // Add a new review
+        public void AddReview(Review review)
+        {
+            _context.Reviews.Add(review);
+            _context.SaveChanges();
+        }
+        //Get list of review 
+        public List<Review> GetAllReview()
+        {
+            return _context.Reviews.ToList();
+
+        }
+        // Get review by ID
+        public Review GetReviewById(int id)
+        {
+            return _context.Reviews.FirstOrDefault(p => p.Id == id);
+        }
+        //delete review 
+        public void DeleteReview(int id)
+        {
+            var review = _context.Reviews.FirstOrDefault(p => p.Id == id);
+            if (review != null)
+            {
+                _context.Reviews.Remove(review);
+                _context.SaveChanges();
+            }
+        }
+        // Update an existing review
+        public void UpdateReview(Review updatedReview)
+        {
+            var existingReview = _context.Reviews.FirstOrDefault(r => r.Id == updatedReview.Id);
+            if (existingReview != null)
+            {
+                existingReview.Rating = updatedReview.Rating;
+                existingReview.Comment = updatedReview.Comment;
+                existingReview.ReviewDate = DateTime.UtcNow; // Update to current timestamp
+                _context.SaveChanges();
+            }
+        }
+    }
+}
