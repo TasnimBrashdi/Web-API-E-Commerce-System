6da614e28fccd49dc6f31f689a1fa5acb1b55920
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-16 14:28:27 +0400

    Add DBcontext

---
+++ E-Commerce-System-API/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using E_Commerce_System_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_System_API
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                        .HasIndex(e => e.Email)
+                        .IsUnique(); //Email Is Unique
+
+
+            // Configure many-to-many relationship between Order and Product
+            modelBuilder.Entity<OrderProducts>()
+             .HasKey(op => new { op.OrderId, op.ProductId });
+
+            modelBuilder.Entity<OrderProducts>()
+                .HasOne(op => op.Order)
+                .WithMany(o => o.OrderProduct)
+                .HasForeignKey(op => op.OrderId);
+
+            modelBuilder.Entity<OrderProducts>()
+                .HasOne(op => op.Product)
+                .WithMany(p => p.OrderProduct)
+                .HasForeignKey(op => op.ProductId);
+        }
+
+    }
+}
