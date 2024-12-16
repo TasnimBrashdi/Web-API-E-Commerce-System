668c062b6a5bf1b97e31f13c79e0ef230ad9c1ea
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-16 15:51:29 +0400

    Add order repo

---
+++ E-Commerce-System-API/Repositories/IOrderRepo.cs
@@ -0,0 +1,12 @@
+﻿using E_Commerce_System_API.Models;
+
+namespace E_Commerce_System_API.Repositories
+{
+    public interface IOrderRepo
+    {
+        void AddOrder(Order order);
+        void DeleteOrder(int id);
+        List<Order> GetAllOrder();
+        Order GetOrderById(int id);
+    }
+}
---
+++ E-Commerce-System-API/Repositories/OrderRepo.cs
@@ -0,0 +1,42 @@
+﻿using E_Commerce_System_API.Models;
+
+namespace E_Commerce_System_API.Repositories
+{
+    public class OrderRepo : IOrderRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+        }
+        //Get list of order 
+        public List<Order> GetAllOrder()
+        {
+            return _context.Orders.ToList();
+
+        }
+        // Get order by ID
+        public Order GetOrderById(int id)
+        {
+            return _context.Orders.FirstOrDefault(p => p.Id == id);
+        }
+        //delete order
+        public void DeleteOrder(int id)
+        {
+            var order = _context.Orders.FirstOrDefault(p => p.Id == id);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+                _context.SaveChanges();
+            }
+        }
+
+    }
+}
