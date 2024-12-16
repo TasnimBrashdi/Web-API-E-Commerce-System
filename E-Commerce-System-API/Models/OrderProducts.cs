ff03f81e8ca55509ddc7586a8e0de9819e0b105d
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-16 14:05:33 +0400

    Add OrderProducts Model

---
+++ E-Commerce-System-API/Models/OrderProducts.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net;
+using System.Text.Json.Serialization;
+
+namespace E_Commerce_System_API.Models
+{
+    [PrimaryKey(nameof(OrderId), nameof(ProductId))]
+    public class OrderProducts
+    {
+        [ForeignKey("Order")]
+        public int OrderId { get; set; }
+        [JsonIgnore]
+        public virtual Order Order { get; set; }
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        [JsonIgnore]
+        public virtual Product Product { get; set; }
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be a non-negative integer.")]
+        public int Quantity { get; set; }   
+
+    }
+}
