50581e692cd43bdc2db151c15351f84fcf4abbcc
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-16 15:21:42 +0400

    Add product repo

---
+++ E-Commerce-System-API/Repositories/IProductRepo.cs
@@ -0,0 +1,14 @@
+﻿using E_Commerce_System_API.Models;
+
+namespace E_Commerce_System_API.Repositories
+{
+    public interface IProductRepo
+    {
+        void AddProduct(Product product);
+        void DeleteProduct(int id);
+        List<Product> GetAllProducts();
+        Product GetProductById(int id);
+        List<Product> GetProductsByName(string name);
+        void UpdateProduct(Product updatedProduct);
+    }
+}
---
+++ E-Commerce-System-API/Repositories/ProductRepo.cs
@@ -0,0 +1,69 @@
+﻿using E_Commerce_System_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_System_API.Repositories
+{
+    public class ProductRepo : IProductRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+        }
+        //Get list of products 
+        public List<Product> GetAllProducts()
+        {
+            return _context.Products.ToList();
+
+        }
+        // Get product by ID
+        public Product GetProductById(int id)
+        {
+            return _context.Products.FirstOrDefault(p => p.PId == id);
+        }
+
+        // Get product by Name
+        public List<Product> GetProductsByName(string name)
+        {
+            return _context.Products
+                .Where(p => EF.Functions.Like(p.Name, $"%{name}%"))
+                .ToList();
+        }
+
+        // Update an existing product
+        public void UpdateProduct(Product updatedProduct)
+        {
+            var existingProduct = _context.Products.FirstOrDefault(p => p.PId == updatedProduct.PId);
+            if (existingProduct != null)
+            {
+                existingProduct.Name = updatedProduct.Name;
+                existingProduct.Description = updatedProduct.Description;
+                existingProduct.Price = updatedProduct.Price;
+                existingProduct.Stock = updatedProduct.Stock;
+
+
+                _context.SaveChanges();
+            }
+        }
+
+        // Delete a product
+        public void DeleteProduct(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.PId == id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+        }
+
+
+    }
+}
