657272754ed2c023f25834c92eb95a550000c8f0
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-16 14:31:42 +0400

    Get Connection wirh sql server

--- E-Commerce-System-API/Program.cs
+++ E-Commerce-System-API/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace E_Commerce_System_API
 {
     public class Program
@@ -6,7 +8,8 @@ namespace E_Commerce_System_API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Add services to the container.
 
             builder.Services.AddControllers();
