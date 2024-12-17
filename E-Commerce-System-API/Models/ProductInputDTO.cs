d7afe88fe935b5e865b729afd032fb13b9f494f5
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-17 13:14:21 +0400

    Add DTO for products

---
+++ E-Commerce-System-API/Models/ProductInputDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce_System_API.Models
+{
+    public class ProductInputDTO
+    {
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0, 1000, ErrorMessage = "Price should be from 0 to 1000")]
+        public decimal Price { get; set; }
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Stock must be a non-negative integer.")]
+        public int Stock { get; set; }
+    }
+}
---
+++ E-Commerce-System-API/Models/ProductOutputDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Commerce_System_API.Models
+{
+    public class ProductOutputDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public decimal Overall_Rating { get; set; }
+    }
+}
