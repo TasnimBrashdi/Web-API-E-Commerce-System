c08353e87a5727caed6012b3040b693e4ca71aff
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-17 12:45:47 +0400

    Add DTO for user

---
+++ E-Commerce-System-API/Models/UserOutput.cs
@@ -0,0 +1,12 @@
+﻿namespace E_Commerce_System_API.Models
+{
+    public class UserOutput
+    {
+        public int UId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
