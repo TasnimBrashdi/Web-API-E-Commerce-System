8c0d9a457271abf88111dd68076be496a062d6cf
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-19 15:08:26 +0400

    Add Review DTOs

---
+++ E-Commerce-System-API/Models/DTO/ReviewInput.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce_System_API.Models.DTO
+{
+    public class ReviewInput
+    {
+        [Required]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int Rating { get; set; }
+
+        [Required]
+        public string Comment { get; set; } = string.Empty;
+
+        [Required]
+        public int ProductId { get; set; }
+    }
+}
