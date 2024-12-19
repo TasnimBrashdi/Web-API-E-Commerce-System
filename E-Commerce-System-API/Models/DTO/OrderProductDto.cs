d65cbe0fe2f614694c89af1fb48f89799093761d
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-19 10:22:33 +0400

    Add DTOs for order

---
+++ E-Commerce-System-API/Models/DTO/OrderProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Commerce_System_API.Models.DTO
+{
+    public class OrderProductDto
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ E-Commerce-System-API/Models/DTO/PlaceOrderDto.cs
@@ -0,0 +1,7 @@
+﻿namespace E_Commerce_System_API.Models.DTO
+{
+    public class PlaceOrderDto
+    {
+        public List<OrderProductDto> Products { get; set; }
+    }
+}
